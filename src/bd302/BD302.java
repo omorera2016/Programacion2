520bc20883d426c104bd742b715c049d11a6519c
StivenCastro138 <84600867+StivenCastro138@users.noreply.github.com>
2023-04-25 14:13:26 -0500

    Update BD302.java
    Optimización

--- src/bd302/BD302.java
+++ src/bd302/BD302.java
@@ -1,19 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bd302;
-
-/**
- *
- * @author Administrador
- */
 public class BD302 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Ventana vn = new Ventana();
         vn.setVisible(true);
